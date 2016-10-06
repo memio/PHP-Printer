011c39865fb44ff4e157b8790552563f350fa81b
Loïc Faugeron <faugeron.loic@gmail.com>
2016-10-06 22:31:30 +0100

    Created TemplatingEngine
    `TemplatingEngine` has been defined as an interface. PHP Printer will provide
    out of the box an implementation using an in-house simple templating language
    named Simpla, but contributions are welome to provide implementations for
    third party ones (such as Twig, Blade, Smarty, etc).
    
    It defines a `render` method which will:
    
    1. get a template using the given name
    2. replace the template's placeholders by the values from the given parameters
    3. return the resulting string
    
    If no templates are found, it will throw a `TemplateNotFound` exception.

---
+++ src/Memio/PhpPrinter/TemplateNotFound.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the memio/PHP-Printer package.
+ *
+ * (c) Loïc Faugeron <faugeron.loic@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Memio\PhpPrinter;
+
+class TemplateNotFound extends \DomainException
+{
+}
