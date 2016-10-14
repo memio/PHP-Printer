00311f0613bb32fb956e88fc070c3e617304f65c
Loïc Faugeron <faugeron.loic@gmail.com>
2016-10-14 18:25:43 +0100

    Created Build
    Dependency Injection means boring and tiresome object construction, which could
    be serious impediment (both in usage and documentation). Our solution to that
    issue is `Build`.
    
    `Build` isn't mandatory, nobody have to use it. It's just here to get started.

---
+++ src/Memio/PhpPrinter/Build.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the memio/PHP-Printer package.
+ *
+ * (c) Loïc Faugeron <faugeron.loic@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Memio\PhpPrinter;
+
+use Memio\PhpPrinter\Simpla\Filesystem\FileGetContentsSimplaFilesystem;
+use Memio\PhpPrinter\Simpla\Rule\ComposedSimplaRule;
+use Memio\PhpPrinter\Simpla\SimplaTemplateCollection;
+use Memio\PhpPrinter\Simpla\SimplaTemplatingEngine;
+
+class Build
+{
+    private $composedSimplaRule;
+    private $simplaTemplateCollection;
+    private $simplaTemplatingEngine;
+
+    public function composedSimplaRule() : ComposedSimplaRule
+    {
+        if (null === $this->composedSimplaRule) {
+            $this->composedSimplaRule = new ComposedSimplaRule();
+        }
+
+        return $this->composedSimplaRule;
+    }
+
+    public function simplaTemplateCollection() : SimplaTemplateCollection
+    {
+        if (null === $this->simplaTemplateCollection) {
+            $this->simplaTemplateCollection = new SimplaTemplateCollection();
+        }
+
+        return $this->simplaTemplateCollection;
+    }
+
+    public function simplaTemplatingEngine() : SimplaTemplatingEngine
+    {
+        if (null === $this->simplaTemplatingEngine) {
+            $this->simplaTemplatingEngine = new SimplaTemplatingEngine(
+                new FileGetContentsSimplaFilesystem(),
+                $this->composedSimplaRule(),
+                $this->simplaTemplateCollection()
+            );
+        }
+
+        return $this->simplaTemplatingEngine;
+    }
+}
